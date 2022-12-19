a1d28f85ddd0be1022be862b6455b25f5567f40a
Oliver Kusch <oliver.kusch@isgus.de>
2022-12-19 09:35:01 +0100

    Adding 500 Error Page

--- pages/movieDetail/[movieId].js
+++ pages/movieDetail/[movieId].js
@@ -1,29 +1,19 @@
 import MovieDetail from "../../components/MovieDetail/MovieDetail";
-import { DUMMY_DATA } from "../../lib/DUMMY_DATA";
 import { getMovieById } from "../../lib/data-fetching";
 
 function MovieDetailPage({ movie }) {
-    const results = DUMMY_DATA; 
-    console.log(movie.success)
-    if (movie.success === 'false') {
-      return(
-        <div className="py-20">
-        <p>Movie could not be found.</p>
-    </div>
-      )
-    }
+
   return (
     <div className="py-20">
-        <MovieDetail movie={movie} />
+      <MovieDetail movie={movie} />
     </div>
-  )
+  );
 }
 
 export async function getServerSideProps(context) {
   const id = context.query.movieId;
-  //const request = [];
   const request = await getMovieById(id);
-  console.log("Movie >>> ", request)
+  console.log("Movie >>> ", request);
 
   return {
     props: {
@@ -32,4 +22,4 @@ export async function getServerSideProps(context) {
   };
 }
 
-export default MovieDetailPage
+export default MovieDetailPage;
