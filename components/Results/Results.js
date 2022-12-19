a9a337b625392d090b1c205c4f1ac2bc8e334535
Oliver Kusch <oliver.kusch@isgus.de>
2022-12-19 07:49:40 +0100

    Fixing results

--- components/Results/Results.js
+++ components/Results/Results.js
@@ -4,7 +4,7 @@ import FlipMove from "react-flip-move";
 
 function Results({ results }) {
   console.log("Results >>> ",results)
-  const data = results;
+  const data = results.results;
   return (
     <div className="px-2 my-10 grid grid-cols-1 md:grid-cols-2 md:px-5 xl:grid-cols-3 3xl:flex flex-wrap justify-center">
         {data.map(result => (
